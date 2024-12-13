9c6185f2e4c75d8d76bf3df89f4a5e51dbef1d49
unknown <HP@DESKTOP-Q8EIMN2>
2024-12-14 04:06:35 +0530

    Updated after debugging error

--- autolysis.py
+++ autolysis.py
@@ -199,7 +199,7 @@ def llm_vision_image_to_text(url_image):
     # output = result['choices'][0]['message']['content']
 
     if 'choices' in result and len(result['choices'])>0:
-        output = result['chioces'][0]['message']['content']
+        output = result['choices'][0]['message']['content']
     else:
         output = "Not proper return format :("
     return output
